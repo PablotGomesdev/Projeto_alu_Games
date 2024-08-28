9164e8570ccde560f6692c16ebbb4c75b276c236
PablotGomesdev <pablotgomesdev@gmail.com>
2024-08-28 19:58:31 -0300

    inserindo o while para evitar repetição de um número aleatório

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,15 +3,20 @@ function sortear () {
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
-//estrutura de repetição utilizando o for
     let sorteados = [];
     let numero;
-
+//estrutura de repetição utilizando o for
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+//utilizando o while para não retornar número repetido
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }        
         sorteados.push(numero);
     }
-    alert(sorteados);
+    //exibindo na tela o resultado
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
 }
 
 function obterNumeroAleatorio(min, max) {
