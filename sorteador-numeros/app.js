d7ba7b30a52153d086fe51cae76a9615a90bd357
PablotGomesdev <pablotgomesdev@gmail.com>
2024-08-28 19:29:27 -0300

    Atualização

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,19 @@
+//sortear
+function sortear () {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+//estrutura de repetição utilizando o for
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
